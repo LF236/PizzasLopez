9eb17f0b35acd989dfbe3194d127b6556f3e8564
LF236-WINDOWS <fernandorodriguez236@gmail.com>
2021-08-08 00:52:03 -0500

    Se agrega un icono nuevo que solo es un circulo, y se agrega un 'Dock' en el carrusel, es un detalle estetico

--- JS/carousel/movingCarousel.js
+++ JS/carousel/movingCarousel.js
@@ -1,14 +1,23 @@
 let buttonLeft = document.getElementById('button-left');
 let buttonRight = document.getElementById('button-right');
-
+let i = 1;
 buttonRight.addEventListener('click', e => {
     let con = document.getElementById('content-aux');
     let positionNow = getComputedStyle(con).getPropertyValue("--movingCarousel");
     positionNow = parseInt(positionNow.replace("%",""));
     if(positionNow > -500) {
         positionNow -= 100;
-        console.log(positionNow);
         con.style.setProperty("--movingCarousel",`${positionNow}%`);    
+
+        //Esta sección es para hacer el efecto del iterador
+        i++;
+        const idElementActual = `images-i-element-${i}`;
+        const auxElementActual = document.getElementById(idElementActual);
+        auxElementActual.style = 'color: yellow';
+        const idElementAnterior = `images-i-element-${i - 1}`;
+        const auxElementAnterior = document.getElementById(idElementAnterior);
+        auxElementAnterior.style = 'color: #fff';
+        //console.log(i);
     }
     
 });
@@ -20,5 +29,15 @@ buttonLeft.addEventListener('click', e => {
     if(positionNow < 0) {
         positionNow += 100;
         con.style.setProperty("--movingCarousel",`${positionNow}%`);
+
+        //Sección para el efecto del iterador
+        const idElementActual = `images-i-element-${i}`;
+        const auxElementActual = document.getElementById(idElementActual);
+        auxElementActual.style = 'color: white';
+        const idElementAnterior = `images-i-element-${i - 1}`;
+        const auxElementAnterior = document.getElementById(idElementAnterior);
+        auxElementAnterior.style = 'color: yellow';
+        i--;
+        console.log(i);
     }
 });
